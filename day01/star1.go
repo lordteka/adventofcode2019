c80d5450dd4bbd1b3fde183d430b7ae8352fc0b2
lordteka <lordteka@users.noreply.github.com>
2019-12-01 23:36:52 +0100

    2 first stars in go :tada: let’s go ! :smirk:

---
+++ day01/star1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "fmt"
+  "bufio"
+  "os"
+  "strconv"
+)
+
+func star1() {
+  file, _ := os.Open("input")
+  scanner := bufio.NewScanner(file)
+  var sum int
+  for scanner.Scan() {
+    num, _ := strconv.Atoi(scanner.Text())
+    sum += (num / 3) - 2
+  }
+  fmt.Println(sum)
+}
