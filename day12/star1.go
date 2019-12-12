ab9cc93c16900728e366656e06ad915464d3c3c7
lordteka <lordteka@users.noreply.github.com>
2019-12-13 00:12:28 +0100

    we did it reddit

---
+++ day12/star1.go
@@ -0,0 +1,93 @@
+// +build star1
+
+package main
+
+import (
+	"fmt"
+)
+
+type Position struct {
+	X int
+	Y int
+	Z int
+}
+
+type Velocity Position
+
+type Moon struct {
+	Pos Position
+	Vel Velocity
+}
+
+// really don’t want to parse the input
+var Moons [4]Moon = [4]Moon{
+	Moon{Position{-10, -13, 7}, Velocity{0, 0, 0}},
+	Moon{Position{1, 2, 1}, Velocity{0, 0, 0}},
+	Moon{Position{-15, -3, 13}, Velocity{0, 0, 0}},
+	Moon{Position{3, 7, -4}, Velocity{0, 0, 0}},
+}
+
+func update_velocity() {
+	for i1 := range Moons {
+		for i2 := range Moons {
+			if i1 == i2 { continue }
+			if Moons[i1].Pos.X > Moons[i2].Pos.X {
+				Moons[i1].Vel.X--
+			} else if Moons[i1].Pos.X < Moons[i2].Pos.X {
+				Moons[i1].Vel.X++
+			}
+			if Moons[i1].Pos.Y > Moons[i2].Pos.Y {
+				Moons[i1].Vel.Y--
+			} else if Moons[i1].Pos.Y < Moons[i2].Pos.Y {
+				Moons[i1].Vel.Y++
+			}
+			if Moons[i1].Pos.Z > Moons[i2].Pos.Z {
+				Moons[i1].Vel.Z--
+			} else if Moons[i1].Pos.Z < Moons[i2].Pos.Z {
+				Moons[i1].Vel.Z++
+			}
+		}
+	}
+}
+
+func update_position() {
+	for i := range Moons {
+		Moons[i].Pos.X += Moons[i].Vel.X
+		Moons[i].Pos.Y += Moons[i].Vel.Y
+		Moons[i].Pos.Z += Moons[i].Vel.Z
+	}
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func potential_energy(m Moon) int {
+	return abs(m.Pos.X) + abs(m.Pos.Y) + abs(m.Pos.Z)
+}
+
+func kinetic_energy(m Moon) int {
+	return abs(m.Vel.X) + abs(m.Vel.Y) + abs(m.Vel.Z)
+}
+
+func total_energy(m Moon) int {
+	return potential_energy(m) * kinetic_energy(m)
+}
+
+func system_energy() (energy int) {
+	for _, m := range Moons {
+		energy += total_energy(m)
+	}
+	return
+}
+
+func main() {
+	for i := 0; i < 1000; i++ {
+		update_velocity()
+		update_position()
+	}
+	fmt.Println(system_energy())
+}
---
+++ day12/star2.go
@@ -0,0 +1,145 @@
+// +build star2
+
+package main
+
+import (
+	"fmt"
+)
+
+type Position struct {
+	X int
+	Y int
+	Z int
+}
+
+type Velocity Position
+
+type Moon struct {
+	Pos Position
+	Vel Velocity
+}
+
+// really don’t want to parse the input
+var Moons [4]Moon = [4]Moon{
+	Moon{Position{-10, -13, 7}, Velocity{0, 0, 0}},
+	Moon{Position{1, 2, 1}, Velocity{0, 0, 0}},
+	Moon{Position{-15, -3, 13}, Velocity{0, 0, 0}},
+	Moon{Position{3, 7, -4}, Velocity{0, 0, 0}},
+}
+
+func update_velocity_x() {
+	for i1 := range Moons {
+		for i2 := range Moons {
+			if i1 == i2 { continue }
+			if Moons[i1].Pos.X > Moons[i2].Pos.X {
+				Moons[i1].Vel.X--
+			} else if Moons[i1].Pos.X < Moons[i2].Pos.X {
+				Moons[i1].Vel.X++
+			}
+		}
+	}
+}
+
+func update_velocity_y() {
+	for i1 := range Moons {
+		for i2 := range Moons {
+			if i1 == i2 { continue }
+			if Moons[i1].Pos.Y > Moons[i2].Pos.Y {
+				Moons[i1].Vel.Y--
+			} else if Moons[i1].Pos.Y < Moons[i2].Pos.Y {
+				Moons[i1].Vel.Y++
+			}
+		}
+	}
+}
+
+func update_velocity_z() {
+	for i1 := range Moons {
+		for i2 := range Moons {
+			if i1 == i2 { continue }
+			if Moons[i1].Pos.Z > Moons[i2].Pos.Z {
+				Moons[i1].Vel.Z--
+			} else if Moons[i1].Pos.Z < Moons[i2].Pos.Z {
+				Moons[i1].Vel.Z++
+			}
+		}
+	}
+}
+
+func update_position_x() {
+	for i := range Moons {
+		Moons[i].Pos.X += Moons[i].Vel.X
+	}
+}
+
+func update_position_y() {
+	for i := range Moons {
+		Moons[i].Pos.Y += Moons[i].Vel.Y
+	}
+}
+
+func update_position_z() {
+	for i := range Moons {
+		Moons[i].Pos.Z += Moons[i].Vel.Z
+	}
+}
+
+func find_x_cycle() (count int) {
+	update_velocity_x()
+	update_position_x()
+	count++
+	// probably check for half a rotation instead of a full one
+	for Moons[0].Vel.X != 0 || Moons[1].Vel.X != 0 || Moons[2].Vel.X != 0 || Moons[3].Vel.X != 0 {
+		update_velocity_x()
+		update_position_x()
+		count++
+	}
+	return
+}
+
+func find_y_cycle() (count int) {
+	update_velocity_y()
+	update_position_y()
+	count++
+	for Moons[0].Vel.Y != 0 || Moons[1].Vel.Y != 0 || Moons[2].Vel.Y != 0 || Moons[3].Vel.Y != 0 {
+		update_velocity_y()
+		update_position_y()
+		count++
+	}
+	return
+}
+
+func find_z_cycle() (count int) {
+	update_velocity_z()
+	update_position_z()
+	count++
+	for Moons[0].Vel.Z != 0 || Moons[1].Vel.Z != 0 || Moons[2].Vel.Z != 0 || Moons[3].Vel.Z != 0 {
+		update_velocity_z()
+		update_position_z()
+		count++
+	}
+	return
+}
+
+func pgcd(a, b int) int {
+	c := a % b
+	if c == 0 {
+		return b
+	}
+	return pgcd(b, c)
+}
+
+func ppcm(a, b int) int {
+	if a > b {
+		return (a * b) / pgcd(a, b)
+	}
+	return (a * b) / pgcd(b, a)
+}
+
+func main() {
+	cycle_x := find_x_cycle()
+	cycle_y := find_y_cycle()
+	cycle_z := find_z_cycle()
+
+	fmt.Println(ppcm(ppcm(cycle_x, cycle_y), cycle_z) * 2)
+}
